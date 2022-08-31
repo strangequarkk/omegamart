1b73dcb948576d7f14ca04659716ebf78e060ef0
Kae Unterseher <turningtern@gmail.com>
2022-08-30 17:35:25 -0700

    progress on jest fetch issues, not quite resolved

--- omega-mart/src/tests/App.test.js
+++ omega-mart/src/tests/App.test.js
@@ -1,13 +1,19 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from '../App.js';
 import '@testing-library/jest-dom';
 
+jest.mock("../../../server/routes/main.js");
+global.fetch = jest.fn();
+jest.spyOn(global, 'fetch').mockResolvedValue(() => {
+  console.log("spying?");
+})
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello omegamart api!/i);
-  expect(linkElement).toBeInTheDocument();
+test('shows loading message before calling the api', async () => {
+  render(<App />); 
+  const loader = screen.getByText("calling the api...");
+  expect(loader).toBeInTheDocument();
+  await waitFor(() => expect(screen.getByText("Hello omegamart api!")).toBeInTheDocument());
 });
